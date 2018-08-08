32706f9c316088cc0010c3948b65847e57960d98
David Phenicie <phenicie@hazmat-1.com>
2018-08-08 15:52:45 -0400

    Updated gradle -- Added actions to watch for changes on login page

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/** @format */
-
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 
-AppRegistry.registerComponent('manager_app', () => App);
+AppRegistry.registerComponent('manager', () => App);
--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount(){
+     // Initialize Firebase
+     const config = {
+       apiKey: 'AIzaSyDKkeYPpM9VtSsghm0xsLWoG3Ux8PXe-XU',
+       authDomain: 'manager-baf3f.firebaseapp.com',
+       databaseURL: 'https://manager-baf3f.firebaseio.com',
+       projectId: 'manager-baf3f',
+       storageBucket: 'manager-baf3f.appspot.com',
+       messagingSenderId: '952154575054'
+     };
+     firebase.initializeApp(config);
+  }
   render(){
     return(
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            Hello!!
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
