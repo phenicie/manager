5a6d15aca401dd368803ffd2f2e4e6c4f80ad24c
David Phenicie <phenicie@hazmat-1.com>
2018-08-09 16:02:43 -0400

    Add action for logging in user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
-
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
+ } from './types';
+import firebase from 'firebase';
 export const emailChanged = (text) => {
   return {
     type: EMAIL_CHANGED,
@@ -13,4 +18,33 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   };
+};
+
+
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    //dispatch({ type: LOGIN_USER_SUCCESS });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
+      });
+  };
+};
+
+
+
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAIL });
 };
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
-import { emailChanged, passwordChanged }  from '../actions';
+import { emailChanged, passwordChanged, loginUser }  from '../actions';
 
 class LoginForm extends Component {
   onEmailChange(text){
@@ -10,6 +11,23 @@ class LoginForm extends Component {
   onPasswordChange(text){
     this.props.passwordChanged(text);
   }
+
+  onButtonPress(){
+    const { email, password } = this.props;
+    this.props.loginUser({ email, password });
+  }
+renderError() {
+  if(this.props.error){
+    return(
+      <View style={{ backgroundColor: 'white'}}>
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
+      </View>
+    );
+  }
+}
+
   render() {
     return(
       <Card>
@@ -29,9 +47,10 @@ class LoginForm extends Component {
             onChangeText={this.onPasswordChange.bind(this)}
             value={ this.props.password }
           />
+        {this.renderError()}
         </CardSection>
         <CardSection>
-         <Button>
+         <Button onPress={this.onButtonPress.bind(this)}>
           Login
          </Button>
         </CardSection>
@@ -41,11 +60,22 @@ class LoginForm extends Component {
   }
 }
 
+const styles = {
+  errorTextStyle:  {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+}
+
 const mapStateToProps = state => {
   return {
     email: state.auth.email,
     password: state.auth.password,
+    error: state.auth.error
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser,
+})(LoginForm);
